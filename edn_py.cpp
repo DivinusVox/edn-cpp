005bade2535c9a8bee24811d0ccbd973dfa8cc22
divinusvox <cole.kacey@gmail.com>
2013-10-24 20:52:54 -0700

    NodeType enum declaration

--- edn_py.cpp
+++ edn_py.cpp
@@ -4,6 +4,7 @@
 #include <boost/python.hpp>
 
 using namespace std;
+using edn::EdnNode;
 
 namespace wrapper
 {
@@ -24,6 +25,27 @@ namespace wrapper
     BOOST_PYTHON_MODULE(edn_py)
     {
         using namespace boost::python;
+
+        enum_<edn::NodeType>("NodeType")
+            .value("EdnNil", edn::EdnNil)
+            .value("EdnSymbol", edn::EdnSymbol)
+            .value("EdnKeyword", edn::EdnKeyword)
+            .value("EdnBool", edn::EdnBool)
+            .value("EdnInt", edn::EdnInt)
+            .value("EdnFloat", edn::EdnFloat)
+            .value("EdnString", edn::EdnString)
+            .value("EdnChar", edn::EdnChar)
+            .value("EdnList", edn::EdnList)
+            .value("EdnVector", edn::EdnVector)
+            .value("EdnMap", edn::EdnMap)
+            .value("EdnSet", edn::EdnSet)
+            .value("EdnDiscard", edn::EdnDiscard)
+            .value("EdnTagged", edn::EdnTagged)
+        ;
+
+        /*class_<edn::EdnNode>("EdnNode")
+            .add_property("type", edn::&EdnNode::type)
+        ;*/
         def("read", read);
     }
 }
